277918b15ae1db210d34b6efef9227f5c0f28bb2
harrywan <harrywan@webank.com>
2021-12-07 15:53:46 +0800

    feat: radio组件

---
+++ components/radio-group/radio-group.vue
@@ -0,0 +1,47 @@
+<template>
+    <div :class="classList">
+        <slot />
+    </div>
+</template>
+<script>
+import { computed } from 'vue';
+import getPrefixCls from '../_util/getPrefixCls';
+import { useRadioGroup } from './useRadioGroup';
+import { UPDATE_MODEL_EVENT, CHANGE_EVENT } from '../_util/constants';
+import { name } from './const';
+
+const prefixCls = getPrefixCls('radio-group');
+export default {
+    name,
+    componentName: name,
+    props: {
+        modelValue: {
+            type: [String, Number, Boolean],
+            default: null,
+        },
+        vertical: {
+            type: Boolean,
+            default: false,
+        },
+        disabled: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    emits: [
+        UPDATE_MODEL_EVENT,
+        CHANGE_EVENT,
+    ],
+    setup(props, ctx) {
+        useRadioGroup(props, ctx);
+        const classList = computed(() => [
+            prefixCls,
+            props.vertical && 'is-vertical',
+            props.disabled && 'is-disabled',
+        ]);
+        return {
+            classList,
+        };
+    },
+};
+</script>
---
+++ components/radio/radio.vue
@@ -0,0 +1,83 @@
+<template>
+    <label :class="wrapperClass" @click="handleClick" @mouseover="handleMouseOver" @mouseout="handleMouseOut">
+        <span :class="`${prefixCls}-inner`" />
+        <span :class="`${prefixCls}-content`">
+            <slot>{{label}}</slot>
+        </span>
+    </label>
+</template>
+<script>
+import { computed } from 'vue';
+import getPrefixCls from '../_util/getPrefixCls';
+import useSelect from '../_util/use/useSelect';
+import { UPDATE_MODEL_EVENT, CHANGE_EVENT } from '../_util/constants';
+import { name, radioGroupKey } from '../radio-group/const';
+
+const prefixCls = getPrefixCls('radio');
+
+export default {
+    name: 'FRadio',
+    componentName: 'FRadio',
+    props: {
+        modelValue: {
+            type: Boolean,
+            default: false,
+        },
+        value: {
+            type: [String, Number, Boolean],
+            default: null,
+        },
+        label: {
+            type: [String, Number, Boolean],
+            default: null,
+        },
+        disabled: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    emits: [
+        UPDATE_MODEL_EVENT,
+        CHANGE_EVENT,
+    ],
+    setup(props, ctx) {
+        const {
+            isGroup,
+            group,
+            hover,
+            checked,
+            disabled,
+            handleClick,
+            handleMouseOver,
+            handleMouseOut,
+        } = useSelect({ props, ctx, parent: { groupKey: radioGroupKey, name } });
+        const wrapperClass = computed(() => {
+            const arr = [`${prefixCls}`];
+            if (checked.value) {
+                arr.push('is-checked');
+            }
+            if (disabled.value) {
+                arr.push('is-disabled');
+            }
+            if (hover.value) {
+                arr.push('is-hover');
+            }
+            if (isGroup.value) {
+                arr.push('is-item');
+                if (group.props.vertical) {
+                    arr.push('is-item-vertical');
+                }
+            }
+            return arr;
+        });
+
+        return {
+            prefixCls,
+            wrapperClass,
+            handleClick,
+            handleMouseOver,
+            handleMouseOut,
+        };
+    },
+};
+</script>
