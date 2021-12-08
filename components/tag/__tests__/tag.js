2baf4571095acbaa5a776318d0e1b05557a73582
ocean_gao <gaopandyx@163.com>
2021-12-08 15:44:47 +0800

    feat: tag

---
+++ components/tag/__tests__/tag.js
@@ -0,0 +1,64 @@
+import { mount } from '@vue/test-utils';
+import { nextTick } from 'vue';
+
+import FTag from '../tag';
+import getPrefixCls from '../../_util/getPrefixCls';
+
+const prefixCls = getPrefixCls('tag');
+
+// ---------------- tag type -------------------
+
+function getTypeWrapper(type) {
+    return mount(FTag, {
+        props: {
+            type,
+        },
+    });
+}
+
+['default', 'success', 'info', 'warning', 'danger'].forEach((type) => {
+    test(`tag type ${type}`, () => {
+        const wrapper = getTypeWrapper(type);
+        const className = `${prefixCls}-type--${type}`;
+        expect(wrapper.classes(className)).toBe(true);
+    });
+});
+
+// ---------------- tag closable -------------------
+
+test('tag closable', async () => {
+    const Closable = {
+        components: { FTag },
+        template: `
+            <FTag @close="handleClose">Default</FTag>
+            <FTag v-if="visible" @close="handleClose" closable type="success">Success</FTag>
+        `,
+        data() {
+            return {
+                visible: true,
+            };
+        },
+        methods: {
+            handleClose() {
+                this.visible = false;
+                this.$emit('close');
+            },
+        },
+    };
+    const wrapper = mount(Closable);
+
+    expect(wrapper.find(`.${prefixCls}-type--default`).exists()).toBe(true);
+    expect(wrapper.find(`.${prefixCls}-type--success`).exists()).toBe(true);
+
+    expect(wrapper.find(`.${prefixCls}-type--default .${prefixCls}__close`).exists()).toBe(false);
+    expect(wrapper.find(`.${prefixCls}-type--success .${prefixCls}__close`).exists()).toBe(true);
+
+    wrapper.find(`.${prefixCls}-type--success .${prefixCls}__close`).trigger('click');
+
+    await nextTick();
+
+    const closeEvent = wrapper.emitted('close');
+    expect(closeEvent).toHaveLength(1);
+
+    expect(wrapper.find(`.${prefixCls}-type--success`).exists()).toBe(false);
+});
---
+++ components/tag/index.js
@@ -0,0 +1,6 @@
+import { withInstall } from '../_util/withInstall';
+import Tag from './tag';
+
+export const FTag = withInstall(Tag);
+
+export default FTag;
