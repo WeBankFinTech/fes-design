5456d02fed53af5c5d4056101098c6b70c7309d6
wanchun <445436867@qq.com>
2021-12-29 19:19:26 +0800

    feat: select支持虚拟滚动

--- components/select/optionList.jsx
+++ components/select/optionList.jsx
@@ -1,6 +1,7 @@
 import { defineComponent } from 'vue';
 import Scrollbar from '../scrollbar';
 import Ellipsis from '../ellipsis';
+import VirtualList from '../virtual-list';
 import CheckOutlined from '../icon/CheckOutlined';
 import { noop } from '../_util/utils';
 
@@ -51,41 +52,56 @@ export default defineComponent({
             return null;
         };
 
+        const renderOption = (option) => {
+            const value = option.value;
+            const isSelected = props.isSelect(value);
+            const prefixCls = `${props.prefixCls}-option`;
+            const classList = [
+                prefixCls,
+                isSelected && 'is-checked',
+                option.disabled && 'is-disabled',
+            ].filter(Boolean);
+            return (
+                <div
+                    class={classList}
+                    onClick={() => {
+                        if (option.disabled) {
+                            return;
+                        }
+                        props.onSelect(value);
+                    }}
+                >
+                    {renderLabel(option, isSelected, prefixCls)}
+                </div>
+            );
+        };
+
         const renderList = () =>
             props.options.length ? (
-                props.options.map((option) => {
-                    const value = option.value;
-                    const isSelected = props.isSelect(value);
-                    const prefixCls = `${props.prefixCls}-option`;
-                    const classList = [
-                        prefixCls,
-                        isSelected && 'is-checked',
-                        option.disabled && 'is-disabled',
-                    ].filter(Boolean);
-                    return (
-                        <div
-                            class={classList}
-                            onClick={() => {
-                                if (option.disabled) {
-                                    return;
-                                }
-                                props.onSelect(value);
-                            }}
-                        >
-                            {renderLabel(option, isSelected, prefixCls)}
-                        </div>
-                    );
-                })
+                props.options.map((option) => renderOption(option))
             ) : (
                 <div class={`${props.prefixCls}-null`}>{props.emptyText}</div>
             );
-        return () => (
-            <Scrollbar
-                containerStyle={props.containerStyle}
-                containerClass={`${props.prefixCls}-dropdown`}
-            >
-                {renderList()}
-            </Scrollbar>
-        );
+
+        const renderDefault = ({ source }) => renderOption(source);
+
+        return () =>
+            props.options.length > 50 ? (
+                <VirtualList
+                    dataSources={props.options}
+                    dataKey={'value'}
+                    estimateSize={32}
+                    keeps={14}
+                    class={`${props.prefixCls}-dropdown is-max-height`}
+                    v-slots={{ default: renderDefault }}
+                ></VirtualList>
+            ) : (
+                <Scrollbar
+                    containerStyle={props.containerStyle}
+                    containerClass={`${props.prefixCls}-dropdown`}
+                >
+                    {renderList()}
+                </Scrollbar>
+            );
     },
 });
