531e9ab7247becedb475483d858a0ec902f3a576
winixt <haizekuo@gmail.com>
2022-01-08 16:01:52 +0800

    chore: 迁移动画代码

--- components/switch/switch.vue
+++ components/switch/switch.vue
@@ -1,92 +1,154 @@
 <template>
     <div :class="wrapperClass" @click="toggle">
         <span :class="`${prefixCls}-inner`">
-            <slot v-if="actived" name="active"></slot>
-            <slot v-if="inactived" name="inactive"></slot>
+            <slot v-if="delayActived" name="active"></slot>
+            <slot v-if="delayInActived" name="inactive"></slot>
         </span>
     </div>
 </template>
 
-<script setup lang="ts">
-import { computed, watch, onMounted } from 'vue';
+<script lang="ts">
+import {
+    defineComponent,
+    computed,
+    watch,
+    onMounted,
+    ref,
+    PropType,
+    Ref,
+} from 'vue';
 import { isEqual, isFunction } from 'lodash-es';
 import { useTheme } from '../_theme/useTheme';
 import getPrefixCls from '../_util/getPrefixCls';
 import { useNormalModel } from '../_util/use/useModel';
-
+import { useAnimate } from '../_util/use/useAnimate';
 import { CHANGE_EVENT } from '../_util/constants';
 
-import type { VModelEvent, ChangeEvent } from '../_util/interface';
-
 const prefixCls = getPrefixCls('switch');
+
+const animateDuration = 300;
+
+const useDelaySwtich = (actived: Ref<boolean>, inactived: Ref<boolean>) => {
+    const delayActived = ref(actived.value);
+    watch(actived, () => {
+        setTimeout(() => {
+            delayActived.value = actived.value;
+        }, animateDuration / 2);
+    });
+    const delayInActived = ref(inactived.value);
+    watch(inactived, () => {
+        setTimeout(() => {
+            delayInActived.value = inactived.value;
+        }, animateDuration / 2);
+    });
+
+    return {
+        delayActived,
+        delayInActived,
+    };
+};
 
 type SwitchValue = string | [] | object | number | boolean;
 type SwitchSize = 'normal' | 'small';
 
-interface SwitchProps {
-    modelValue?: SwitchValue;
-    disabled?: boolean;
-    activeValue?: SwitchValue;
-    inactiveValue?: SwitchValue;
-    beforeChange?: (val: SwitchValue) => boolean | Promise<boolean>;
-    size?: SwitchSize;
-}
+// interface SwitchProps {
+//     modelValue?: SwitchValue;
+//     disabled?: boolean;
+//     activeValue?: SwitchValue;
+//     inactiveValue?: SwitchValue;
+//     beforeChange?: (val: SwitchValue) => boolean | Promise<boolean>;
+//     size?: SwitchSize;
+// }
 
-type SwitchEmits = {
-    (e: VModelEvent, value: SwitchValue): void;
-    (e: ChangeEvent, value: SwitchValue): void;
-};
+// type SwitchEmits = {
+//     (e: VModelEvent, value: SwitchValue): void;
+//     (e: ChangeEvent, value: SwitchValue): void;
+// };
 
-const props = withDefaults(defineProps<SwitchProps>(), {
-    disabled: false,
-    activeValue: true,
-    inactiveValue: false,
-    size: 'normal',
-});
+const switchProps = {
+    modelValue: {
+        type: [String, Array, Object, Number, Boolean] as PropType<SwitchValue>,
+    },
+    disabled: {
+        type: Boolean,
+        default: false,
+    },
+    activeValue: {
+        type: [String, Array, Object, Number, Boolean] as PropType<SwitchValue>,
+        default: true,
+    },
+    inactiveValue: {
+        type: [String, Array, Object, Number, Boolean] as PropType<SwitchValue>,
+        default: false,
+    },
+    beforeChange: Function as PropType<
+        (val: SwitchValue) => boolean | Promise<boolean>
+    >,
+    size: {
+        type: String as PropType<SwitchSize>,
+        default: 'normal',
+    },
+} as const;
 
-const emit = defineEmits<SwitchEmits>();
-
-useTheme();
-const [currentValue, updateCurrentValue] = useNormalModel(props, emit, {
-    prop: 'modelValue',
-    isEqual: true,
-});
-onMounted(() => {
-    // 默认为未选中
-    if (currentValue.value === null) {
-        updateCurrentValue(props.inactiveValue);
-    }
-});
-
-watch(currentValue, () => {
-    emit(CHANGE_EVENT, currentValue.value);
-});
-const actived = computed(() => isEqual(currentValue.value, props.activeValue));
-const inactived = computed(() =>
-    isEqual(currentValue.value, props.inactiveValue),
-);
-const toggle = async () => {
-    if (props.disabled) return;
-    if (isFunction(props.beforeChange)) {
-        const confirm = await props.beforeChange(currentValue.value);
-        if (!confirm) {
-            return;
-        }
-    }
-    updateCurrentValue(actived.value ? props.inactiveValue : props.activeValue);
-};
-const wrapperClass = computed(() =>
-    [
-        prefixCls,
-        props.size && `${prefixCls}-size-${props.size}`,
-        actived.value && 'is-checked',
-        props.disabled && 'is-disabled',
-    ].filter(Boolean),
-);
-</script>
-
-<script lang="ts">
-export default {
+export default defineComponent({
     name: 'FSwitch',
-};
+    props: switchProps,
+    setup(props, ctx) {
+        const { handelAnimate, animateClassName } = useAnimate(animateDuration);
+        useTheme();
+        const [currentValue, updateCurrentValue] = useNormalModel(
+            props,
+            ctx.emit,
+            {
+                prop: 'modelValue',
+                isEqual: true,
+            },
+        );
+        onMounted(() => {
+            // 默认为未选中
+            if (currentValue.value === null) {
+                updateCurrentValue(props.inactiveValue);
+            }
+        });
+
+        watch(currentValue, () => {
+            ctx.emit(CHANGE_EVENT, currentValue.value);
+        });
+        const actived = computed(() =>
+            isEqual(currentValue.value, props.activeValue),
+        );
+        const inactived = computed(() =>
+            isEqual(currentValue.value, props.inactiveValue),
+        );
+
+        const toggle = async () => {
+            if (props.disabled) return;
+            handelAnimate();
+            if (isFunction(props.beforeChange)) {
+                const confirm = await props.beforeChange(currentValue.value);
+                if (!confirm) {
+                    return;
+                }
+            }
+            updateCurrentValue(
+                actived.value ? props.inactiveValue : props.activeValue,
+            );
+        };
+        const wrapperClass = computed(() =>
+            [
+                prefixCls,
+                animateClassName.value,
+                props.size && `${prefixCls}-size-${props.size}`,
+                actived.value && 'is-actived',
+                props.disabled && 'is-disabled',
+            ].filter(Boolean),
+        );
+        return {
+            prefixCls,
+            wrapperClass,
+            toggle,
+            ...useDelaySwtich(actived, inactived),
+        };
+    },
+});
 </script>
