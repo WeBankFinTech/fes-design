6df0c46c066f374da2eceb15737b4b70f1811e87
wanchun <445436867@qq.com>
2022-03-18 17:28:38 +0800

    test: checkboxgroup

--- components/checkbox-group/__tests__/checkbox-group.jsx
+++ components/checkbox-group/__tests__/checkbox-group.jsx
@@ -1,66 +1,55 @@
 import { mount } from '@vue/test-utils';
 import CheckboxGroup from '../checkbox-group.vue';
 import Checkbox from '../../checkbox/checkbox.vue';
-import getPrefixCls from '../../_util/getPrefixCls';
 
-const prefixCls = getPrefixCls('checkbox-group');
+const TRIGGER_CLASS = 'trigger';
 
 const _mount = (props, slots = {}) =>
     mount(CheckboxGroup, {
         props,
-        slots,
-    });
-
-describe('Checkbox', () => {
-    test('FCheckbox default', async () => {
-        const wrapper = _mount(
-            {},
-            {
+        slots: {
+            ...{
                 default: () => (
                     <>
-                        <Checkbox class={'trigger'} value={1}>
+                        <Checkbox class={TRIGGER_CLASS} value={1}>
                             1
                         </Checkbox>
                         <Checkbox value={2}>2</Checkbox>
                     </>
                 ),
             },
-        );
+            ...slots,
+        },
+    });
+
+describe('FCheckboxGroup', () => {
+    test('FCheckboxGroup default', async () => {
+        const wrapper = _mount();
         await wrapper.find(`.trigger`).trigger('click');
         expect(wrapper.emitted()).toHaveProperty('change');
         expect(wrapper.emitted()['change'][0][0]).toEqual([1]);
-        // const wrapper = mount(CheckboxGroup, {
-        //     props: {},
-        // });
-        // await wrapper.find(`.${prefixCls}`).trigger('click');
-        // expect(wrapper.classes('is-checked')).toBe(true);
-        // expect(wrapper.emitted()).toHaveProperty('change');
-        // await wrapper.find(`.${prefixCls}`).trigger('click');
-        // expect(wrapper.classes('is-checked')).toBe(false);
-        // await wrapper.find(`.${prefixCls}`).trigger('mouseover');
-        // expect(wrapper.classes('is-hover')).toBe(true);
-        // await wrapper.find(`.${prefixCls}`).trigger('mouseout');
-        // expect(wrapper.classes('is-hover')).toBe(false);
+        expect(wrapper.emitted()).toHaveProperty('update:modelValue');
+        expect(wrapper.emitted()['update:modelValue'][0][0]).toEqual([1]);
+        await wrapper.find(`.trigger`).trigger('click');
+        expect(wrapper.emitted()).toHaveProperty('change');
+        expect(wrapper.emitted()['change'][1][0]).toEqual([]);
+        expect(wrapper.emitted()).toHaveProperty('update:modelValue');
+        expect(wrapper.emitted()['update:modelValue'][1][0]).toEqual([]);
     });
 
-    // test('FCheckbox indeterminate', async () => {
-    //     const wrapper = mount(Checkbox, {
-    //         props: {
-    //             indeterminate: true,
-    //         },
-    //     });
-    //     expect(wrapper.classes('is-indeterminate')).toBe(true);
-    // });
+    test('FCheckboxGroup vertical', async () => {
+        const wrapper = _mount({
+            vertical: true,
+        });
+        expect(wrapper.classes('is-vertical')).toBe(true);
+    });
 
-    // test('FCheckbox disabled', async () => {
-    //     const wrapper = mount(Checkbox, {
-    //         props: {
-    //             disabled: true,
-    //         },
-    //     });
-    //     expect(wrapper.classes('is-disabled')).toBe(true);
-    //     await wrapper.find(`.${prefixCls}`).trigger('click');
-    //     expect(wrapper.classes('is-checked')).toBe(false);
-    //     expect(wrapper.emitted()).not.toHaveProperty('change');
-    // });
+    test('FCheckboxGroup disabled', async () => {
+        const wrapper = _mount({
+            disabled: true,
+        });
+        expect(wrapper.classes('is-disabled')).toBe(true);
+        await wrapper.find(`.${TRIGGER_CLASS}`).trigger('click');
+        expect(wrapper.emitted()).not.toHaveProperty('change');
+    });
 });
