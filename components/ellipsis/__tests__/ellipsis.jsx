b968926460761bf84dbe4c3fce520837113aceb9
wanchun <445436867@qq.com>
2022-03-28 14:16:11 +0800

    refactor: 重构ellipsis,更适合单元测试

--- components/ellipsis/__tests__/ellipsis.jsx
+++ components/ellipsis/__tests__/ellipsis.jsx
@@ -1,1 +1,80 @@
 import { mount } from '@vue/test-utils';
+import FEllipsis from '../ellipsis.tsx';
+import { sleep } from '../../_util/utils';
+
+import getPrefixCls from '../../_util/getPrefixCls';
+import { nextTick } from 'vue';
+
+const prefixCls = getPrefixCls('ellipsis');
+
+describe('FEllipsis', () => {
+    test('should work with import on demand', () => {
+        mount(FEllipsis);
+    });
+
+    test('should work with base', async () => {
+        const wrapper = mount(FEllipsis, {
+            props: { style: 'max-width: 240px;' },
+            slots: {
+                default: () =>
+                    '住在我心里孤独的 孤独的海怪 痛苦之王 开始厌倦 深海的光 停滞的海浪',
+            },
+        });
+
+        expect(wrapper.find(`.${prefixCls}`).exists()).toBe(true);
+        expect(wrapper.find(`.${prefixCls}`).attributes('style')).toContain(
+            'text-overflow: ellipsis;',
+        );
+    });
+
+    test('should work with `line-clamp` prop', async () => {
+        const wrapper = mount(FEllipsis, {
+            props: { line: 2 },
+            slots: {
+                default: () => (
+                    <div>
+                        电灯熄灭 物换星移 泥牛入海
+                        <br />
+                        黑暗好像 一颗巨石 按在胸口
+                        <br />
+                        独脚大盗 百万富翁 摸爬滚打
+                    </div>
+                ),
+            },
+        });
+
+        expect(wrapper.find(`.${prefixCls}`).classes()).toContain(
+            'is-line-clamp',
+        );
+    });
+
+    test('tooltip', async () => {
+        const wrapper = mount(FEllipsis, {
+            props: { style: 'max-width: 240px;', tooltip: { showAfter: 0 } },
+            slots: {
+                default: () =>
+                    '住在我心里孤独的 孤独的海怪 痛苦之王 开始厌倦 深海的光 停滞的海浪',
+            },
+            attachTo: 'body',
+        });
+
+        await wrapper.find(`.${prefixCls}`).trigger('mouseenter');
+
+        await nextTick();
+
+        console.log(document.body.innerHTML);
+
+        expect(
+            document.body.querySelector('.fes-popper-wrapper').innerHTML,
+        ).not.toBe('');
+
+        await wrapper.setProps({
+            tooltip: false,
+        });
+
+        await wrapper.find(`.${prefixCls}`).trigger('mouseenter');
+        expect(
+            document.body.querySelector('.fes-popper-wrapper').innerHTML,
+        ).toBe('');
+    });
+});
