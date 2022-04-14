55059e61e15d6f3887769309bc8b40f238699cc9
tinatian <tinatian@webamk.com>
2022-04-14 14:16:22 +0800

    refactor: 1.干掉setup语法糖 2.新增复杂校验案例 3.暴露FFormItem的clearValidate

--- components/form/form.vue
+++ components/form/form.vue
@@ -4,8 +4,8 @@
     </form>
 </template>
 
-<script setup lang="ts">
-import { provide, toRefs, computed } from 'vue';
+<script lang="ts">
+import { provide, toRefs, computed, defineComponent } from 'vue';
 import {
     provideKey,
     FORM_LAYOUT,
@@ -15,132 +15,138 @@ import {
 import getPrefixCls from '../_util/getPrefixCls';
 import { useTheme } from '../_theme/useTheme';
 import { allPromiseFinish } from './utils';
-
+import { FORM_NAME } from './const';
 import type { Field, ValidateResult } from './interface';
 
 const prefixCls = getPrefixCls('form');
 
-interface FormProps {
-    model?: object;
-    rules?: object;
-    layout?: FORM_LAYOUT;
-    labelPosition?: LABEL_POSITION;
-    showMessage?: boolean;
-    labelWidth?: string | number;
-    labelClass?: string;
-}
+const formProps = {
+    model: Object,
+    rules: {
+        type: Object,
+        default: () => ({}),
+    },
+    layout: {
+        type: String,
+        default: FORM_LAYOUT.HORIZONTAL,
+    },
+    labelPosition: {
+        type: String,
+        default: LABEL_POSITION.LEFT,
+    },
+    showMessage: {
+        type: Boolean,
+        default: true,
+    },
+    labelWidth: String | Number,
+    labelClass: String,
+} as const;
 
-const props = withDefaults(defineProps<FormProps>(), {
-    layout: FORM_LAYOUT.HORIZONTAL,
-    labelPosition: LABEL_POSITION.LEFT,
-    showMessage: true,
-});
 
-useTheme();
-
-const formFields: {
-    [key: string]: Field;
-} = {};
-const formClass = computed(() => [prefixCls, `${prefixCls}-${props.layout}`]);
-
-const addField = (formItemProp: string, formItemContext: Field) => {
-    formItemProp && (formFields[formItemProp] = formItemContext);
-};
-const removeField = (formItemProp: string) => {
-    delete formFields[formItemProp];
-};
-
-const validateFields = async (
-    fieldProps?: string[],
-    trigger = TRIGGER_TYPE_DEFAULT,
-) => {
-    if (!props.model)
-        return Promise.reject(
-            'Form `model` is required for resetFields to work.',
-        );
-    const specifyPropsFlag: boolean = Boolean(fieldProps.length); // 是否指定prop: 【部分】表单字段校验调用会指定; 【整个】表单校验调用则不会指定
-
-    const promiseList: Promise<any>[] = []; // 原始校验结果
-    Object.values(formFields).forEach((formField) => {
-        if (!formField.rules.length) return; // Skip if without rule
-
-        if (specifyPropsFlag && !fieldProps.includes(formField.prop)) return; // Skip if Specify prop but not include
-
-        const promise = formField.validateRules(trigger);
-        promiseList.push(
-            promise
-                .then(() => ({ name: formField.prop, errors: [] }))
-                .catch((errors) =>
-                    Promise.reject({ name: formField.prop, errors }),
-                ),
-        );
-    });
-    const summaryPromise = allPromiseFinish(promiseList); // 汇总校验结果
-
-    try {
-        await summaryPromise;
-    } catch (results: any) {
-        const errorList: ValidateResult[] = [];
-        const errorNameList: string[] = [];
-        results.length &&
-            results.forEach((result: ValidateResult) => {
-                if (result && result.errors.length) {
-                    errorList.push(result);
-                    errorNameList.push(result.name);
-                }
-            });
-        return Promise.reject({
-            valid: false,
-            values: errorNameList,
-            errorFields: errorList,
-        });
-    }
-};
-
-/** 表单校验
- *    fieldProps { string[] }    指定校验字段的 props 数组
- *    return    { Promise }   校验结果
- */
-const validate = (fieldProps = []) => validateFields(fieldProps);
-
-// 移除表单项的校验结果
-const clearValidate = () => {
-    if (!props.model)
-        return Promise.reject(
-            'Form `model` is required for resetFields to work.',
-        );
-    Object.values(formFields).forEach((formField) => {
-        formField.clearValidate();
-    });
-};
-
-// 对整个表单进行重置，将所有字段值重置为初始值并移除校验结果
-const resetFields = () => {
-    if (!props.model)
-        return Promise.reject(
-            'Form `model` is required for resetFields to work.',
-        );
-    Object.values(formFields).forEach((formField) => {
-        formField.resetField();
-    });
-};
-
-provide(provideKey, {
-    ...toRefs(props),
-    addField,
-    removeField,
-});
-
-defineExpose({
-    validate,
-    clearValidate,
-    resetFields,
-});
-</script>
-
-<script lang="ts">
-import { FORM_NAME } from './const';
-export default {
+export default defineComponent({
     name: FORM_NAME,
-};
+    props: formProps,
+    setup(props, { slots, emit }) {
+        useTheme();
+
+        const formFields: {
+            [key: string]: Field;
+        } = {};
+        const formClass = computed(() => [prefixCls, `${prefixCls}-${props.layout}`]);
+
+        const addField = (formItemProp: string, formItemContext: Field) => {
+            formItemProp && (formFields[formItemProp] = formItemContext);
+        };
+        const removeField = (formItemProp: string) => {
+            delete formFields[formItemProp];
+        };
+        const validateFields = async (
+            fieldProps?: string[],
+            trigger = TRIGGER_TYPE_DEFAULT,
+        ) => {
+            if (!props.model)
+                return Promise.reject(
+                    'Form `model` is required for resetFields to work.',
+                );
+            const specifyPropsFlag: boolean = Boolean(fieldProps.length); // 是否指定prop: 【部分】表单字段校验调用会指定; 【整个】表单校验调用则不会指定
+            const promiseList: Promise<any>[] = []; // 原始校验结果
+
+            Object.values(formFields).forEach((formField) => {
+                console.log('formField', formField.prop, formField);
+                if (specifyPropsFlag && !fieldProps.includes(formField.prop)) return; // Skip if Specify prop but not include
+
+                const promise = formField.validateRules(trigger);
+                promiseList.push(
+                    promise
+                        .then(() => ({ name: formField.prop, errors: [] }))
+                        .catch((errors) =>
+                            Promise.reject({ name: formField.prop, errors }),
+                        ),
+                );
+            });
+            const summaryPromise = allPromiseFinish(promiseList); // 汇总校验结果
+
+            try {
+                await summaryPromise;
+            } catch (results: any) {
+                const errorList: ValidateResult[] = [];
+                const errorNameList: string[] = [];
+                results.length &&
+                    results.forEach((result: ValidateResult) => {
+                        if (result && result.errors.length) {
+                            errorList.push(result);
+                            errorNameList.push(result.name);
+                        }
+                    });
+                return Promise.reject({
+                    valid: false,
+                    values: errorNameList,
+                    errorFields: errorList,
+                });
+            }
+        };
+
+        /** 表单校验
+         *    fieldProps { string[] }    指定校验字段的 props 数组
+         *    return    { Promise }   校验结果
+         */
+        const validate = (fieldProps = []) => validateFields(fieldProps);
+
+        /** 移除表单项的校验结果 */
+        const clearValidate = () => {
+            if (!props.model)
+                return Promise.reject(
+                    'Form `model` is required for resetFields to work.',
+                );
+            Object.values(formFields).forEach((formField) => {
+                formField.clearValidate();
+            });
+        };
+
+        /** 对整个表单进行重置，将所有字段值重置为初始值并移除校验结果 */
+        const resetFields = () => {
+            if (!props.model)
+                return Promise.reject(
+                    'Form `model` is required for resetFields to work.',
+                );
+            Object.values(formFields).forEach((formField) => {
+                formField.resetField();
+            });
+        };
+
+        provide(provideKey, {
+            ...toRefs(props),
+            addField,
+            removeField,
+        });
+
+        return {
+            formClass,
+
+            validate,
+            clearValidate,
+            resetFields,
+        };
+    }
+})
 </script>
---
+++ docs/.vitepress/components/form/complexValidate.vue
@@ -0,0 +1,228 @@
+<template>
+    <FForm
+        ref="WFormDomRef"
+        labelWidth="140px"
+        labelPosition="right"
+        :model="modelForm"
+        :rules="rules"
+    >
+        <div class="complex-validate-item"><MoreCircleFilled /> 自定义 trigger、validator 场景: </div>
+        <FFormItem label="密码" prop="password">
+            <FInput
+                v-model="modelForm.password"
+                type="password"
+                showPassword
+                placeholder="请输入密码"
+                @input="handlePasswordInput"
+            ></FInput>
+        </FFormItem>
+        <FFormItem ref="rePasswordRef" label="再次输入" prop="rePassword">
+            <FInput
+                v-model="modelForm.rePassword"
+                type="password"
+                showPassword
+                placeholder="请再次输入密码"
+            ></FInput>
+        </FFormItem>
+
+        <div class="complex-validate-item"><MoreCircleFilled /> 联动校验场景: </div>
+        <FFormItem label="类型选择" prop="type">
+            <FRadioGroup v-model="modelForm.type">
+                <FRadio value="admin">admin</FRadio>
+                <FRadio value="edit">edit</FRadio>
+                <FRadio value="run">run</FRadio>
+            </FRadioGroup>
+        </FFormItem>
+        <!-- 在 FForm 维度【全局rules】绑定联动规则  -->
+        <FFormItem v-if="modelForm.type === 'admin'" label="admin 详情" prop="adminDesc">
+            <FInput
+                v-model="modelForm.adminDesc"
+                placeholder="请输入adminDesc【在 FForm 维度绑定联动规则】"
+            ></FInput>
+        </FFormItem>
+        <!-- 在 FFormItem 维度绑定联动规则  -->
+        <FFormItem v-if="modelForm.type === 'edit'" label="edit 详情" prop="editDesc" :rules="modelForm.type === 'edit' ? [{required: true, message: '请输入editDesc'}] : []">
+            <FInput
+                v-model="modelForm.editDesc"
+                placeholder="请输入editDesc【在 FFormItem 维度绑定联动规则】"
+            ></FInput>
+        </FFormItem>
+
+        <div class="complex-validate-item"><MoreCircleFilled /> v-for 任意添加选项场景校验: </div>
+        <FFormItem
+            v-for="(item, index) in modelForm.options"
+            :key="index"
+            :label="item.label"
+            :prop="`options[${index}].value`"
+            :rules="[{ required: index % 2 !== 0, message: '请输入选项值' }]"
+        >
+            <FSpace>
+                <FInput
+                    v-model="modelForm.options[index].value"
+                    placeholder="请输入选项"
+                ></FInput>
+                <PlusSquareOutlined @click="addOptionItem" />
+            </FSpace>
+        </FFormItem>
+
+        <div class="complex-validate-item"><MoreCircleFilled /> v-for 动态 ref 校验: </div>
+        <div v-for="(item, index) in modelForm.scenes" :key="index" style="display: flex; margin-left: 65px">
+            {{ `场景${index + 1}名称：` }}
+            <FFormItem :prop="`scenes[${index}].name`">
+                <FInput v-model="modelForm.scenes[index].name" :maxlength="10" @input="validateScenesCont(index)"/>
+            </FFormItem>
+            <span style="margin-left: 10px">内容：</span>
+            <FFormItem
+                :prop="`scenes[${index}].content`"
+                :ref="(el) => (scenesDomRef[index] = el)"
+                :rules="modelForm.scenes[index].name ? [{ required: true, message: '' }] : []"
+            >
+                <FInput v-model="modelForm.scenes[index].content" :maxlength="10" :placeholder="`名称${index + 1}填写，内容必填`"/>
+            </FFormItem>
+        </div>
+
+        <FFormItem label=" ">
+            <FButton type="primary" style="margin-right: 20px" @click="submitHandler">提交</FButton>
+            <FButton type="primary" style="margin-right: 20px" @click="clearHandler">清除</FButton>
+            <FButton type="primary" @click="resetHandler">重置</FButton>
+        </FFormItem>
+    </FForm>
+</template>
+
+<script>
+import { ref, reactive, computed } from 'vue';
+// eslint-disable-next-line import/no-unresolved
+import { FMessage } from '@fesjs/fes-design';
+
+export default {
+    setup() {
+        const WFormDomRef = ref(null);
+        const rePasswordRef = ref(null);
+        const modelForm = reactive({
+            password: '',
+            rePassword: '',
+            type: 'admin',
+            typeDesc: '',
+            options: [
+                { label: '选项1', value: '' },
+            ],
+            scenes: [
+                { name: '', content: '' },
+                { name: '', content: '' },
+            ],
+        });
+        const scenesDomRef = ref([]);
+
+        const validatePasswordStartWith = (rule, value) => {
+            return Boolean(
+                modelForm.password &&
+                    modelForm.password.startsWith(value) &&
+                    modelForm.password.length >= value.length,
+            );
+        };
+        const rules = computed(() => {
+            return {
+                password: [
+                    {
+                        required: true,
+                        message: '请输入密码',
+                        trigger: ['blur', 'change'],
+                    },
+                ],
+                rePassword: [
+                    {
+                        required: true,
+                        message: '请再次输入密码',
+                        trigger: ['change', 'blur'],
+                    },
+                    {
+                        validator: validatePasswordStartWith,
+                        message: '再次输入密码时，两次密码输入不一致',
+                        trigger: ['change'],
+                    },
+                    {
+                        validator: (rule, value) => (value === modelForm.password),
+                        message: '输入密码时，两次密码输入不一致',
+                        trigger: 'password-input',
+                    },
+                ],
+                adminDesc: [
+                    modelForm.type === 'admin' && {
+                        required: true,
+                        message: '请输入adminDesc',
+                        trigger: ['change', 'blur'],
+                    },
+                ],
+            };
+        });
+        
+        // 调用自定义的 password-input trigger
+        const handlePasswordInput = () => {
+            if (modelForm.rePassword) {
+                rePasswordRef.value.validate('password-input');
+            }
+        };
+
+        const addOptionItem = () => {
+            modelForm.options.push({
+                label: `选项${ modelForm.options.length + 1}`,
+                value: '',
+            });
+        };
+
+        const validateScenesCont = (index) => {
+            // 通过 FFormItem 的动态 ref 调用 validate 校验
+            modelForm?.scenes[index]?.name ? scenesDomRef.value[index].validate() : scenesDomRef.value[index].clearValidate();
+        };
+
+        const submitHandler = async () => {
+            // 通过 FForm 的 validate 直接校验
+            try {
+                await WFormDomRef.value.validate();
+                console.log('表单验证成功~');
+            } catch (error) {
+                console.log('表单验证失败: ', error);
+            }
+        };
+        const clearHandler = () => {
+            WFormDomRef.value.clearValidate();
+        };
+        const resetHandler = () => {
+            WFormDomRef.value.resetFields();
+        };
+
+        return {
+            WFormDomRef,
+            rePasswordRef,
+            modelForm,
+            rules,
+            scenesDomRef,
+
+            handlePasswordInput,
+            validateScenesCont,
+            addOptionItem,
+            submitHandler,
+            clearHandler,
+            resetHandler,
+        };
+    },
+};
+</script>
+<style scoped>
+.fes-form {
+    width: 600px;
+}
+/* .fes-form .fes-form-item-label.more-label {
+    color: #9e9e9e;
+}
+.row-container {
+    width: 100%;
+}
+.row-item {
+    display: flex;
+} */
+.complex-validate-item {
+    margin: 10px 0 20px 60px;
+    color: rgb(136 136 136)
+}
+</style>
