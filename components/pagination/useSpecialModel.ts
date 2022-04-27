99bff1efafde4cdf2823566e24b3cbf05231f69c
bobzhang01 <103159899+bobzhang01@users.noreply.github.com>
2022-04-27 11:23:57 +0800

    fix: Pagination 手动设置 currentPage、pageSize 不触发相应事件 (#128)
    * fix: Pagination 手动设置 currentPage, pageSize 不触发相应事件
    
    * docs: 优化 Pagination 文档

---
+++ components/pagination/useSpecialModel.ts
@@ -0,0 +1,54 @@
+import { ref, watch, computed, WritableComputedRef } from 'vue';
+import { isEqual } from 'lodash-es';
+
+export default (
+    props: Record<string, any>,
+    emit: any,
+    config: {
+        prop?: string;
+        isEqual?: boolean;
+    } = {
+        prop: 'modelValue',
+        isEqual: false,
+    },
+    callback: Function
+): [WritableComputedRef<any>, (val: any) => void] => {
+    const usingProp = config?.prop ?? 'modelValue';
+    const currentValue = ref(props[usingProp]);
+    const pureUpdateCurrentValue = (value: any) => {
+        if (
+            value === currentValue.value ||
+            (config.isEqual && isEqual(value, currentValue.value))
+        ) {
+            return;
+        }
+        currentValue.value = value;
+    };
+    const updateCurrentValue = (value: any) => {
+        if (value === currentValue.value) {
+            return;
+        }
+        pureUpdateCurrentValue(value);
+        emit(`update:${usingProp}`, value);
+        callback();
+    };
+
+    watch(
+        () => props[usingProp],
+        (val) => {
+            pureUpdateCurrentValue(val);
+        },
+    );
+
+    return [
+        computed({
+            get() {
+                return currentValue.value;
+            },
+            set(value) {
+                updateCurrentValue(value);
+            },
+        }),
+        updateCurrentValue,
+    ];
+};
