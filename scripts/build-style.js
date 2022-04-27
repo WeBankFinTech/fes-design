17cf25dc77266eda97d4d8e2084f32c26f6e90b8
winixt <haizekuo@gmail.com>
2022-04-27 10:54:48 +0800

    fix: 修复样式引用问题

--- scripts/build-style.js
+++ scripts/build-style.js
@@ -7,7 +7,7 @@ const path = require('path');
 const { compilerCss } = require('./compilerCss');
 const { OUTPUT_DIR } = require('./build-shard');
 
-const STYLE_SOURCE = path.join(__dirname, '../components/style.ts');
+const STYLE_SOURCE = path.join(__dirname, '../components/_style.ts');
 
 fse.mkdirsSync(OUTPUT_DIR);
 
