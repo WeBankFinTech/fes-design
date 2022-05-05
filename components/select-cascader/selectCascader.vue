99c05966205cbe4d8c2b527e350b26513157e44e
ocean-gao <gaopandyx@163.com>
2022-05-05 19:38:30 +0800

    refactor: 代码规范

--- components/select-cascader/selectCascader.vue
+++ components/select-cascader/selectCascader.vue
@@ -229,7 +229,7 @@ export default defineComponent({
                 : [currentValue.value];
 
             // 支持未匹配项展示
-            return values.map((curValue) => {
+            return values.filter(Boolean).map((curValue) => {
                 const { value, label, indexPath, labelPath } = nodeList.value[
                     curValue
                 ] || {
