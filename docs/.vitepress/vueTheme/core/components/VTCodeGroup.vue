85421e9724b792a48bb29650aa209ebfe3351465
winixt <haizekuo@gmail.com>
2022-05-10 12:36:17 +0800

    docs: 优化文档样式，避免影响组件样式

---
+++ docs/.vitepress/vueTheme/core/components/VTCodeGroup.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import { VTCodeGroupTab } from '..';
+import { useSlots, ref } from 'vue';
+const activeTabIndex = ref(0);
+const children = useSlots().default?.();
+const tabs = children?.filter(({ type }) => type === VTCodeGroupTab);
+</script>
+
+<template>
+    <div class="vt-code-group">
+        <div class="vt-code-group-tabs">
+            <div
+                v-for="(tab, idx) in tabs"
+                class="vt-code-group-tab"
+                :class="{
+                    'vt-code-group-tab-active': activeTabIndex === idx,
+                }"
+                @click="activeTabIndex = idx"
+            >
+                {{ tab.props?.label }}
+            </div>
+        </div>
+        <div class="vt-code-group-contents">
+            <component
+                :is="tab"
+                v-for="(tab, idx) in tabs"
+                v-show="activeTabIndex === idx"
+                :active="activeTabIndex === idx"
+            />
+        </div>
+    </div>
+</template>
