053d3e11e59a3b9b04d74cbd0b1cc1041bf13306
wanchun <445436867@qq.com>
2022-06-09 18:38:13 +0800

    feat: ellipsis添加content属性

--- docs/.vitepress/components/ellipsis/common.vue
+++ docs/.vitepress/components/ellipsis/common.vue
@@ -1,38 +1,16 @@
 <template>
-    <FButton @click="handleShow('住在我心里孤独的')">点我</FButton>
-    <FButton
-        @click="
-            handleShow(
-                '住在我心里孤独的 孤独的海怪 痛苦之王 开始厌倦 深海的光 停滞的海浪',
-            )
-        "
-    >
-        点我
-    </FButton>
-    <FModal
-        v-model:show="show[0]"
-        title="这里是标题"
-        displayDirective="show"
-        @ok="show[0] = false"
-    >
-        <FEllipsis :content="text" style="max-width: 240px"> </FEllipsis>
-    </FModal>
+    <FEllipsis :content="text" style="max-width: 240px"> </FEllipsis>
 </template>
 <script>
-import { ref, reactive } from 'vue';
+import { ref } from 'vue';
 export default {
     setup() {
-        const text = ref('');
-        const show = reactive([]);
+        const text = ref(
+            '住在我心里孤独的 孤独的海怪 痛苦之王 开始厌倦 深海的光 停滞的海浪',
+        );
 
-        const handleShow = (_text) => {
-            text.value = _text;
-            show[0] = true;
-        };
         return {
             text,
-            show,
-            handleShow,
         };
     },
 };
