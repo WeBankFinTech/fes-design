f7a42b3bd6758fe411254d4158bbb7140fa107de
harrywan <445436867@qq.com>
2022-08-25 19:22:02 +0800

    fix: flatten节点会配置Fragment包裹节点的key,避免出现渲染异常 (#218)
    * fix: flatten节点会配置Fragment包裹节点的key,避免出现渲染异常
    
    * fix: key要递归下去

--- components/_util/vnode.ts
+++ components/_util/vnode.ts
@@ -45,7 +45,7 @@ export function getSlot(slots: any, slotName = 'default') {
 }
 
 // o(n) flatten
-export function flatten(vNodes: VNode[], result: VNode[] = []) {
+export function flatten(vNodes: VNode[], result: VNode[] = [], key?: string) {
     vNodes.forEach((vNode) => {
         if (vNode === null) return;
         if (typeof vNode !== 'object') {
@@ -60,8 +60,18 @@ export function flatten(vNodes: VNode[], result: VNode[] = []) {
         }
         if (vNode.type === Fragment) {
             if (vNode.children === null) return;
+            const currentKey = key
+                ? `${key}_${String(vNode.key)}`
+                : String(vNode.key);
             if (Array.isArray(vNode.children)) {
-                flatten(vNode.children as VNode[], result);
+                (vNode.children as VNode[]).forEach(
+                    (node: VNode, index: number) => {
+                        if (node.key === undefined || node.key === null) {
+                            node.key = `${currentKey}_${index}`;
+                        }
+                    },
+                );
+                flatten(vNode.children as VNode[], result, currentKey);
             }
             // rawSlot
         } else {
