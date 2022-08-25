28f26f4156d9140764c6b3f0ea9a1a1c76bb4c27
wanchun <445436867@qq.com>
2022-08-25 17:57:59 +0800

    fix: key要递归下去

--- components/_util/vnode.ts
+++ components/_util/vnode.ts
@@ -45,7 +45,7 @@ export function getSlot(slots: any, slotName = 'default') {
 }
 
 // o(n) flatten
-export function flatten(vNodes: VNode[], result: VNode[] = []) {
+export function flatten(vNodes: VNode[], result: VNode[] = [], key?: string) {
     vNodes.forEach((vNode) => {
         if (vNode === null) return;
         if (typeof vNode !== 'object') {
@@ -60,16 +60,18 @@ export function flatten(vNodes: VNode[], result: VNode[] = []) {
         }
         if (vNode.type === Fragment) {
             if (vNode.children === null) return;
-            const key = vNode.key;
+            const currentKey = key
+                ? `${key}_${String(vNode.key)}`
+                : String(vNode.key);
             if (Array.isArray(vNode.children)) {
                 (vNode.children as VNode[]).forEach(
                     (node: VNode, index: number) => {
                         if (node.key === undefined || node.key === null) {
-                            node.key = `${String(key)}_${index}`;
+                            node.key = `${currentKey}_${index}`;
                         }
                     },
                 );
-                flatten(vNode.children as VNode[], result);
+                flatten(vNode.children as VNode[], result, currentKey);
             }
             // rawSlot
         } else {
