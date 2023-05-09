8ed1a756edd3a2adc14f51433247adfc733ed2ec
ocean-gao <gaopandyx@163.com>
2023-05-09 18:02:11 +0800

    feat: grid组件支持响应式布局

--- components/grid/gridItem.vue
+++ components/grid/gridItem.vue
@@ -6,8 +6,9 @@
 
 <script setup lang="ts">
 import { computed, CSSProperties, inject } from 'vue';
-import { GRID_KEY } from './const';
+import { isNumber, isObject, isUndefined } from 'lodash-es';
 import getPrefixCls from '../_util/getPrefixCls';
+import { GRID_KEY } from './const';
 
 const prefixCls = getPrefixCls('grid-item');
 
@@ -23,6 +24,13 @@ function parseFlex(flex: number | string) {
     return flex;
 }
 
+type ColSize = {
+    offset?: number;
+    pull?: number;
+    push?: number;
+    span?: number;
+};
+
 type GridItemProps = {
     flex?: string | number;
     offset?: number;
@@ -30,13 +38,20 @@ type GridItemProps = {
     push?: number;
     span?: number;
     order?: number;
+    xs?: number | ColSize;
+    sm?: number | ColSize;
+    md?: number | ColSize;
+    lg?: number | ColSize;
+    xl?: number | ColSize;
+    xxl?: number | ColSize;
+    xxxl?: number | ColSize;
 };
 
 const props = withDefaults(defineProps<GridItemProps>(), {
     offset: 0,
     pull: 0,
     push: 0,
-    span: 0,
+    span: 24,
     order: 0,
 });
 
@@ -44,15 +59,43 @@ const rowProps = inject(GRID_KEY, {
     gutterX: { value: 0 },
 });
 
-const classList = computed(() =>
-    [
+const classList = computed(() => {
+    const sizes = ['xs', 'sm', 'md', 'lg', 'xl', 'xxl', 'xxxl'] as const;
+
+    const classes: string[] = [
         prefixCls,
         props.offset && `${prefixCls}-offset-${props.offset}`,
         props.pull && `${prefixCls}-pull-${props.pull}`,
         props.push && `${prefixCls}-push-${props.push}`,
         `${prefixCls}-${props.span}`,
-    ].filter(Boolean),
-);
+    ].filter(Boolean);
+
+    let sizeClasses: string[] = [];
+    sizes.forEach((size) => {
+        let sizeProps: ColSize = {};
+        const propSize = props[size];
+        if (isNumber(propSize)) {
+            sizeProps.span = propSize;
+        } else if (isObject(propSize)) {
+            sizeProps = propSize || {};
+        }
+
+        sizeClasses = sizeClasses.concat(
+            [
+                !isUndefined(sizeProps.span) &&
+                    `${prefixCls}-${size}-${sizeProps.span}`,
+                (sizeProps.offset || sizeProps.offset === 0) &&
+                    `${prefixCls}-${size}-offset-${sizeProps.offset}`,
+                (sizeProps.pull || sizeProps.pull === 0) &&
+                    `${prefixCls}-${size}-pull-${sizeProps.pull}`,
+                (sizeProps.push || sizeProps.push === 0) &&
+                    `${prefixCls}-${size}-push-${sizeProps.push}`,
+            ].filter(Boolean),
+        );
+    });
+
+    return classes.concat(sizeClasses);
+});
 const style = computed(() => {
     const _style: CSSProperties = {};
     if (props.flex) {
---
+++ docs/.vitepress/components/grid/responsive.vue
@@ -0,0 +1,13 @@
+<template>
+    <f-grid>
+        <f-grid-item :xs="2" :sm="4" :md="6" :lg="8" :xl="10">
+            <div class="col-demo">col</div>
+        </f-grid-item>
+        <f-grid-item :xs="20" :sm="16" :md="12" :lg="8" :xl="4">
+            <div class="col-demo">col</div>
+        </f-grid-item>
+        <f-grid-item :xs="2" :sm="4" :md="6" :lg="8" :xl="10">
+            <div class="col-demo">col</div>
+        </f-grid-item>
+    </f-grid>
+</template>
