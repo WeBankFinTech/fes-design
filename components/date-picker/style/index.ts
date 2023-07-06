92371c52e9d894d3f0fab676444c3d32c4b79724
qlin <haizekuo@gmail.com>
2023-07-06 11:14:55 +0800

    fix: 修复 date-picker input 样式权重问题 (#338)

--- components/date-picker/style/index.ts
+++ components/date-picker/style/index.ts
@@ -1,6 +1,5 @@
 import '../../style';
 import '../../popper/style';
 import '../../button/style';
-import '../../time-picker/style';
 import '../../input/style/inputInner.less';
 import './index.less';
