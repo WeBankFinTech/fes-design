fd82c110054de9dc15d533aa870e33ac0e417dcf
ocean_gao <gaopandyx@163.com>
2023-08-02 15:30:41 +0800

    docs: input输入框组件补充文档，及增加聚焦和失焦案例 (#371)
    * docs: input输入框组件补充文档，及增加聚焦和失焦案例
    
    * refactor: 去掉多余的附加逻辑

---
+++ docs/.vitepress/components/input/autofocus.vue
@@ -0,0 +1,44 @@
+<template>
+    <FButton class="mb-10" type="primary" @click="handleFocus">
+        手动聚焦
+    </FButton>
+    <FInput ref="inputRef" placeholder="请输入"></FInput>
+</template>
+
+<script>
+import { ref, onMounted, nextTick } from 'vue';
+
+export default {
+    setup() {
+        const inputRef = ref(null);
+
+        // 自动聚焦和失焦
+        onMounted(() => {
+            setTimeout(() => {
+                inputRef.value?.focus();
+            }, 1000);
+
+            setTimeout(() => {
+                inputRef.value?.blur();
+            }, 5000);
+        });
+
+        // 手动聚焦
+        const handleFocus = async () => {
+            await nextTick();
+            inputRef.value?.focus();
+        };
+
+        return {
+            inputRef,
+            handleFocus,
+        };
+    },
+};
+</script>
+
+<style scoped>
+.mb-10 {
+    margin-bottom: 10px;
+}
+</style>
