7de60c91db34088ba3b6dda950ed7acee9419400
ocean-gao <gaopandyx@163.com>
2023-08-15 11:02:58 +0800

    docs: 导航栏增加版本及相关生态链接

--- docs/.vitepress/configs/navbar/index.js
+++ docs/.vitepress/configs/navbar/index.js
@@ -1,3 +1,5 @@
+import { version } from '../../../../package.json';
+
 module.exports = {
     zh: [
         {
@@ -9,6 +11,28 @@ module.exports = {
             text: '组件',
             activeMatch: `/components/`,
             link: '/zh/components/button',
+        },
+        {
+            text: `v${version}`,
+            items: [
+                {
+                    text: '更新日志',
+                    link: 'https://github.com/WeBankFinTech/fes-design/blob/main/CHANGELOG.md',
+                },
+                {
+                    text: 'New issue',
+                    link: 'https://github.com/WeBankFinTech/fes-design/issues/new/choose',
+                },
+            ],
+        },
+        {
+            text: '生态',
+            items: [
+                {
+                    text: 'Fes.js',
+                    link: 'https://fesjs.mumblefe.cn/',
+                },
+            ],
         },
     ],
 };
