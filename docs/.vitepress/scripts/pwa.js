43d5ba88a906f3e7c1d7c852d15818b395966e4a
ocean_gao <gaopandyx@163.com>
2023-08-22 12:51:35 +0800

    docs: pwa优化，支持大文件缓存 (#418)

--- docs/.vitepress/scripts/pwa.js
+++ docs/.vitepress/scripts/pwa.js
@@ -4,6 +4,7 @@ exports.pwa = () => {
         registerType: 'autoUpdate', // 注册类型为自动更新
         includeManifestIcons: false, // 不包含清单图标
         workbox: {
+            maximumFileSizeToCacheInBytes: 1024 * 1024 * 6, // 默认是 1024 * 1024 * 2
             globPatterns: ['**/*.{css,js,html,svg,png,ico,txt,woff2}'], // 匹配需要缓存的文件类型
             runtimeCaching: [],
         },
