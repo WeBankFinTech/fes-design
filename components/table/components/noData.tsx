4f2849aa1556bd1947271332891904ea62b13bc2
ocean-gao <gaopandyx@163.com>
2023-08-31 21:33:35 +0800

    feat(table): 无数据场景交互优化及丰富组件文档无数据的案例

--- components/table/components/noData.tsx
+++ components/table/components/noData.tsx
@@ -3,20 +3,13 @@ import { provideKey } from '../const';
 
 export default defineComponent({
     setup() {
-        const { prefixCls, columns, rootProps, rootCtx } = inject(provideKey);
+        const { prefixCls, rootProps, rootCtx } = inject(provideKey);
 
         return () => {
             return (
-                <tbody>
-                    <tr class={`${prefixCls}-row`}>
-                        <td
-                            colspan={columns.value.length}
-                            class={`${prefixCls}-td ${prefixCls}-cell ${prefixCls}-no-data`}
-                        >
-                            {rootCtx.slots.empty?.() || rootProps.emptyText}
-                        </td>
-                    </tr>
-                </tbody>
+                <div class={`${prefixCls}-no-data`}>
+                    {rootCtx.slots.empty?.() || rootProps.emptyText}
+                </div>
             );
         };
     },
--- docs/.vitepress/components/table/nodata.vue
+++ docs/.vitepress/components/table/nodata.vue
@@ -1,17 +1,92 @@
 <template>
-    <f-table :data="data">
-        <f-table-column prop="date" label="日期"></f-table-column>
-        <f-table-column prop="name" label="姓名"></f-table-column>
-        <f-table-column prop="address" label="地址"></f-table-column>
+    <FForm :labelWidth="160">
+        <FFormItem label="展示类型：">
+            <FRadioGroup v-model="emptyType">
+                <FRadio value="normal">默认</FRadio>
+                <FRadio value="custom">自定义</FRadio>
+            </FRadioGroup>
+        </FFormItem>
+        <FFormItem label="间距大小：">
+            <FRadioGroup v-model="size">
+                <FRadio value="middle">middle(默认)</FRadio>
+                <FRadio value="small">small</FRadio>
+            </FRadioGroup>
+        </FFormItem>
+        <FFormItem label="自定义空数据文本：">
+            <FInput v-model="emptyText"></FInput>
+        </FFormItem>
+    </FForm>
+
+    <FDivider></FDivider>
+
+    <f-table
+        v-if="emptyType === 'normal'"
+        :data="data"
+        :size="size"
+        :emptyText="emptyText"
+        bordered
+    >
+        <f-table-column :width="200" prop="date" label="日期"></f-table-column>
+        <f-table-column :width="200" prop="name" label="姓名"></f-table-column>
+        <f-table-column
+            :width="200"
+            prop="address"
+            label="地址"
+        ></f-table-column>
+        <f-table-column
+            :width="200"
+            prop="contact"
+            label="联系人"
+        ></f-table-column>
+        <f-table-column
+            :width="200"
+            prop="postcode"
+            label="邮编"
+        ></f-table-column>
+    </f-table>
+
+    <f-table
+        v-if="emptyType === 'custom'"
+        :data="data"
+        :size="size"
+        verticalLine
+    >
+        <template #empty>
+            <FEmpty :description="emptyText" />
+        </template>
+        <f-table-column :width="200" prop="date" label="日期"></f-table-column>
+        <f-table-column :width="200" prop="name" label="姓名"></f-table-column>
+        <f-table-column
+            :width="200"
+            prop="address"
+            label="地址"
+        ></f-table-column>
+        <f-table-column
+            :width="200"
+            prop="contact"
+            label="联系人"
+        ></f-table-column>
+        <f-table-column
+            :width="200"
+            prop="postcode"
+            label="邮编"
+        ></f-table-column>
     </f-table>
 </template>
 <script>
-import { reactive } from 'vue';
+import { ref, reactive } from 'vue';
 export default {
     setup() {
+        const emptyType = ref('normal');
+        const size = ref('middle');
+        const emptyText = ref();
+
         const data = reactive([]);
         return {
+            emptyType,
             data,
+            size,
+            emptyText,
         };
     },
 };
