5d9eeb3e89bda2bb8e2623f14b8bb17d7430db59
ocean-gao <gaopandyx@163.com>
2023-08-31 10:21:10 +0800

    refactor: empty组件props代码规范

--- components/empty/props.ts
+++ components/empty/props.ts
@@ -1,28 +1,22 @@
-import type { PropType, StyleValue } from 'vue';
+import type { PropType, StyleValue, ComponentObjectPropsOptions } from 'vue';
 import type { ExtractPublicPropTypes } from '../_util/interface';
 
 export const emptyProps = {
-    /**
-     * @description image URL of empty
-     */
+    // image URL of empty
     imageSrc: {
         type: String,
         default: '',
     },
-    /**
-     * @description image style of empty
-     */
+    // image style of empty
     imageStyle: {
         type: [Object, Array, String] as PropType<StyleValue>,
         default: () => ({}),
     },
-    /**
-     * @description description of empty
-     */
+    // description of empty
     description: {
         type: String,
         default: '',
     },
-} as const;
+} as const satisfies ComponentObjectPropsOptions;
 
 export type EmptyProps = ExtractPublicPropTypes<typeof emptyProps>;
