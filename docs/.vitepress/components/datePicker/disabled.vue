5c0a5ad67e446b447281e117b67a14b6ec2a4695
ocean-gao <gaopandyx@163.com>
2023-09-22 19:37:07 +0800

    docs(date-picker): 补充文档禁用案例

--- docs/.vitepress/components/datePicker/disabled.vue
+++ docs/.vitepress/components/datePicker/disabled.vue
@@ -1,7 +1,18 @@
 <template>
     <FSpace vertical>
         <FDatePicker disabled />
+        <FDatePicker :modelValue="Date.now()" disabled />
         <FDatePicker type="daterange" disabled />
+        <FDatePicker
+            :modelValue="[Date.now(), Date.now() + 7 * 24 * 60 * 60 * 1000]"
+            type="daterange"
+            disabled
+        />
         <FDatePicker type="datemultiple" disabled />
+        <FDatePicker
+            :modelValue="[Date.now(), Date.now() + 7 * 24 * 60 * 60 * 1000]"
+            type="datemultiple"
+            disabled
+        />
     </FSpace>
 </template>
