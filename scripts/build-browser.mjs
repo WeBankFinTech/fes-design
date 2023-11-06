0d513a2f7105fa9ef976afadf9a44dc2ba11e04f
ocean-gao <gaopandyx@163.com>
2023-11-06 15:36:20 +0800

    build: build:esm-browser 脚本 改为 mjs

---
+++ scripts/build-browser.mjs
@@ -0,0 +1,18 @@
+import path from 'path';
+import fse from 'fs-extra';
+import { rollup } from 'rollup';
+
+import { getRollupConfig, OUTPUT_DIR } from './build-shard.mjs';
+
+fse.mkdirsSync(OUTPUT_DIR);
+
+async function compiler() {
+    const bundle = await rollup(getRollupConfig());
+    const outputFilePath = path.join(OUTPUT_DIR, 'fes-design.esm-browser.js');
+    await bundle.write({
+        file: outputFilePath,
+        format: 'esm',
+    });
+}
+
+compiler();
