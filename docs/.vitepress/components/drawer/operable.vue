bb1cc5b37e7a9e09ee7139a6ddc9eaceb45466e8
blankzhang <blankzhang@blankzhangdeMac-mini.local>
2023-12-22 17:46:56 +0800

    feat: 提供联动demo

--- docs/.vitepress/components/drawer/operable.vue
+++ docs/.vitepress/components/drawer/operable.vue
@@ -1,10 +1,22 @@
 <template>
-    <FButton @click="show = true">没有遮罩</FButton>
+    <FSpace>
+        <FForm :labelWidth="100">
+            <FFormItem label="蒙层开关:">
+                <FSwitch v-model="mask"></FSwitch>
+            </FFormItem>
+            <FFormItem label="可交互开关:">
+                <FSwitch v-model="operable"></FSwitch>
+            </FFormItem>
+        </FForm>
+    </FSpace>
+    <FSpace>
+        <FButton @click="show = true">打开抽屉</FButton>
+    </FSpace>
     <FDrawer
         v-model:show="show"
-        title="没有遮罩"
-        :mask="false"
-        :operable="true"
+        title="抽屉组件"
+        :mask="mask"
+        :operable="operable"
         @ok="show = false"
     >
         <div>我是内容...</div>
@@ -16,4 +28,6 @@
 <script setup>
 import { ref } from 'vue';
 const show = ref(false);
+const mask = ref(false);
+const operable = ref(true);
 </script>
