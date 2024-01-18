1dfe8b496ed8dfb9ce1ccac56250f956b3a8c8e0
Ming <66944708+zym19960704@users.noreply.github.com>
2024-01-18 17:57:28 +0800

    fix(DatePicker): 修复季度无法置灰问题 (#599)

--- components/date-picker/useCalendar.ts
+++ components/date-picker/useCalendar.ts
@@ -580,6 +580,8 @@ export const useQuarter = (
         value: number;
     };
 
+    const format = 'yyyy-MM';
+
     const quarterList = [
         {
             name: 'Q1',
@@ -614,7 +616,7 @@ export const useQuarter = (
             (selectedDate) =>
                 selectedDate &&
                 selectedDate.year === currentDate.year &&
-                item.value === selectedDate.month / 3 + 1,
+                item.value === Math.floor(selectedDate.month / 3) + 1,
         );
 
     const isNow = (item: QuarterItem) => {
@@ -625,10 +627,19 @@ export const useQuarter = (
         );
     };
 
+    // 季度置灰判断
+    const isDisabled = (item: QuarterItem) => {
+        const year = currentDate.year;
+        const month = (item.value - 1) * 3;
+        const date = new Date(year, month);
+        return props.disabledDate && props.disabledDate(date, format);
+    };
+
     const quarterCls = (item: QuarterItem) => ({
         [`${prefixCls}-date`]: true,
         [`${prefixCls}-date-selected`]: isSelected(item),
         [`${prefixCls}-date-now`]: isNow(item),
+        [`${prefixCls}-date-disabled`]: isDisabled(item),
     });
     return {
         isQuarterSelect,
