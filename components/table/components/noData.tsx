7fbfe2d77c5bb4ad714853d63ce0968b54476d5e
1zumii <524123601@qq.com>
2024-01-25 19:35:55 +0800

    feat(Table): 无数据文案支持国际化 (#612)

--- components/table/components/noData.tsx
+++ components/table/components/noData.tsx
@@ -1,16 +1,20 @@
 import { defineComponent, inject } from 'vue';
 import { provideKey } from '../const';
+import { useLocale } from '../../config-provider/useLocale';
 
 export default defineComponent({
     setup() {
         const { prefixCls, rootProps, rootCtx } = inject(provideKey);
 
+        const { t } = useLocale();
+
         return () => {
-            return (
-                <div class={`${prefixCls}-no-data`}>
-                    {rootCtx.slots.empty?.() || rootProps.emptyText}
-                </div>
-            );
+            const emptyText =
+                rootCtx.slots.empty?.() ??
+                rootProps.emptyText ??
+                t('empty.emptyText');
+
+            return <div class={`${prefixCls}-no-data`}>{emptyText}</div>;
         };
     },
 });
