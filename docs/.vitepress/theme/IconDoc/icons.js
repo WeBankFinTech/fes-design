26d8b56a3324935523d5f826f247c0f813959bdc
Ming <66944708+zym19960704@users.noreply.github.com>
2024-01-29 17:48:38 +0800

    feat: 新增icon (#616)
    Co-authored-by: blankzhang <blankzhang@blankzhangdeMac-mini.local>

--- docs/.vitepress/theme/IconDoc/icons.js
+++ docs/.vitepress/theme/IconDoc/icons.js
@@ -100,3 +100,9 @@ export { default as UserManagementOutlined } from '../../../../components/icon/U
 export { default as UserOutlined } from '../../../../components/icon/UserOutlined';
 export { default as VerticalLeftOutlined } from '../../../../components/icon/VerticalLeftOutlined';
 export { default as VerticalRightOutlined } from '../../../../components/icon/VerticalRightOutlined';
+export { default as LikeOutlined } from '../../../../components/icon/LikeOutlined';
+export { default as LikeFilled } from '../../../../components/icon/LikeFilled';
+export { default as MailOutlined } from '../../../../components/icon/MailOutlined';
+export { default as PlayOutlined } from '../../../../components/icon/PlayOutlined';
+export { default as PlayFilled } from '../../../../components/icon/PlayFilled';
+export { default as CodeOutlined } from '../../../../components/icon/CodeOutlined';
