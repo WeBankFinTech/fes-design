71ac16ac48cb887da55ff0368e20643a89507586
ocean_gao <gaopandyx@163.com>
2024-02-02 16:51:43 +0800

    docs: vitepress 相关更新还原 (#629)

--- docs/postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-import { postcssIsolateStyles } from 'vitepress';
-
-export default {
-    plugins: [
-        postcssIsolateStyles({
-            includeFiles: [/vp-doc\.css/],
-        }),
-    ],
-};
