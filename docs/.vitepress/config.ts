c5d0fc97d0d0d03b78c1d30c76804dec6d09d7a9
1zumii <524123601@qq.com>
2024-02-29 17:40:59 +0800

    build: 升级 vitepress，修复组件样式被 vitepress 覆盖问题 (#647)

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import { defineConfig } from 'vitepress';
 import { withPwa } from '@vite-pwa/vitepress';
 import { getProjectRootDir } from '../../scripts/utils.mjs';
 import { genComponentDocWatch } from '../../scripts/genComponentDoc.mjs';
@@ -22,81 +23,93 @@ if (process.env.NODE_ENV !== 'production') {
     genComponentDocWatch();
 }
 
-export default withPwa({
-    pwa: pwa(),
-    base: BASE_URL,
-    lang: 'zh-CN',
-    title: 'Fes Design',
-    description: '微众银行中后台设计 Fes Design',
-    appearance: false,
-    // 配置图标
-    head: [
-        [
-            'link',
-            {
-                rel: 'icon',
-                href: '/logo.svg',
-            },
+export default withPwa(
+    defineConfig({
+        pwa: pwa(),
+        base: BASE_URL,
+        lang: 'zh-CN',
+        title: 'Fes Design',
+        description: '微众银行中后台设计 Fes Design',
+        appearance: false,
+        // 配置图标
+        head: [
+            [
+                'link',
+                {
+                    rel: 'icon',
+                    href: '/logo.svg',
+                },
+            ],
         ],
-    ],
-    vite: {
-        define: {
-            __VUE_OPTIONS_API__: false,
-        },
-        server: {
-            watch: {
-                ignored: ['**/docs/.vitepress/components/**'],
+        vite: {
+            define: {
+                __VUE_OPTIONS_API__: false,
+            },
+            server: {
+                watch: {
+                    ignored: ['**/docs/.vitepress/components/**'],
+                },
+            },
+            optimizeDeps: {
+                exclude: ['@vue/repl'],
+            },
+            ssr: {
+                external: ['@vue/repl'],
+                // lodash-es 模块是 esm，ssr 渲染的时候编译成 cjs 的引入方式，会引发 nodejs 的模块加载异常错误
+                noExternal: ['lodash-es'],
+            },
+            resolve: {
+                extensions: [
+                    '.mjs',
+                    '.js',
+                    '.ts',
+                    '.jsx',
+                    '.tsx',
+                    '.json',
+                    '.vue',
+                ],
+                alias: {
+                    '@fesjs/fes-design': path.resolve(rootDir, './components'),
+                },
+            },
+            json: {
+                stringify: true,
+            },
+            plugins: [vueJsx({})],
+            build: {
+                chunkSizeWarningLimit: 1000,
             },
         },
-        ssr: {
-            // lodash-es 模块是 esm，ssr 渲染的时候编译成 cjs 的引入方式，会引发 nodejs 的模块加载异常错误
-            noExternal: ['lodash-es'],
-        },
-        resolve: {
-            extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
-            alias: {
-                '@fesjs/fes-design': path.resolve(rootDir, './components'),
-            },
-        },
-        json: {
-            stringify: true,
-        },
-        plugins: [vueJsx({})],
-        build: {
-            chunkSizeWarningLimit: 1000,
-        },
-    },
-    vue: {
-        template: {
-            compilerOptions: {
-                directiveTransforms: {
-                    drag: ssrTransformCustomDir,
-                    mousewheel: ssrTransformCustomDir,
-                    sticky: ssrTransformCustomDir,
+        vue: {
+            template: {
+                compilerOptions: {
+                    directiveTransforms: {
+                        drag: ssrTransformCustomDir,
+                        mousewheel: ssrTransformCustomDir,
+                        sticky: ssrTransformCustomDir,
+                    },
                 },
             },
         },
-    },
-    themeConfig: {
-        appearance: false,
-        title: 'Fes Design',
-        logo: `${BASE_URL}images/fes-logo.svg`,
-        socialLinks: [
-            {
-                icon: 'github',
-                link: 'https://github.com/WeBankFinTech/fes-design',
+        themeConfig: {
+            logo: `${BASE_URL}images/fes-logo.svg`,
+            socialLinks: [
+                {
+                    icon: 'github',
+                    link: 'https://github.com/WeBankFinTech/fes-design',
+                },
+            ],
+            nav: navbar.zh,
+            sidebar: sidebar.zh,
+
+            outline: {
+                label: '本页目录',
+                level: [2, 3],
             },
-        ],
-        nav: navbar.zh,
-        sidebar: sidebar.zh,
 
-        outline: {
-            label: '本页目录',
-            level: [2, 3],
+            search: {
+                provider: 'local',
+            },
         },
-
-        search: {
-            provider: 'local',
-        },
-    },
-});
+    }),
+);
