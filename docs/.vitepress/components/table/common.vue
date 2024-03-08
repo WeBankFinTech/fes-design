5058149691077d1a040f30725015f007d6b06211
winixt <haizekuo@gmail.com>
2024-03-08 18:46:25 +0800

    chore: 测试代码回退

--- docs/.vitepress/components/table/common.vue
+++ docs/.vitepress/components/table/common.vue
@@ -53,11 +53,7 @@ const data = reactive(
     Array.from([1, 2, 3], (i) => {
         return {
             date: `2016-05-${i < 10 ? '0' + i : i}`,
-            name: [
-                {
-                    name: 1,
-                },
-            ],
+            name: '王小虎',
             address: '上海市普陀区金沙江路 1516 弄',
         };
     }),
