73fc4df794109e3ce9c46e04c0b5fe020eed4a6e
ocean-gao <gaopandyx@163.com>
2024-04-23 17:17:06 +0800

    refactor: commitlint 配置修改

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,1 +1,8 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [2, 'always', [
+          "feat", "fix", "docs", "style", "refactor", "test", "chore", "revert", "release",
+        ]],
+      },
+};
