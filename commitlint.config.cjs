8a7d56bac42df178d72782f0c3ed5fc6614c9e96
ocean-gao <gaopandyx@163.com>
2024-04-23 17:50:29 +0800

    refactor(commitlint): config update

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,8 +1,17 @@
+/**
+ * Type 文档:
+ * https://github.com/angular/angular/blob/22b96b9/CONTRIBUTING.md#type
+ */
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'type-enum': [2, 'always', [
-          "feat", "fix", "docs", "style", "refactor", "test", "chore", "revert", "release",
-        ]],
-      },
+        'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert', 'release', 'build']],
+    },
+    'type-case': [0],
+    'type-empty': [0],
+    'scope-empty': [0],
+    'scope-case': [0],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never'],
+    'header-max-length': [0, 'always', 72],
 };
