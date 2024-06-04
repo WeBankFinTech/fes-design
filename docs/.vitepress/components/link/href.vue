052074e1831483ff9a6189530f03ccf7688c76bc
ocean-gao <gaopandyx@163.com>
2024-06-04 18:16:09 +0800

    feat(Link): 解决嵌套a标签样式冲突的问题

--- docs/.vitepress/components/link/href.vue
+++ docs/.vitepress/components/link/href.vue
@@ -1,3 +1,8 @@
 <template>
-    <FLink href="https://fesjs.mumblefe.cn/" target="_blank">Fes.js官网</FLink>
+    <FSpace>
+        <FLink href="https://fesjs.mumblefe.cn/" target="_blank">Fes.js官网</FLink>
+        <FLink type="primary">
+            <a href="https://fesjs.mumblefe.cn/" target="_blank">Fes.js官网</a>
+        </FLink>
+    </FSpace>
 </template>
