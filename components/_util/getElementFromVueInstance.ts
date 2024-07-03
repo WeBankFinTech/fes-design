7467bb126536b388b39c2a9656b9e6365b2a35e9
ocean-gao <gaopandyx@163.com>
2024-07-03 11:21:30 +0800

    feat(Tooltip): 支持svg元素

--- components/_util/getElementFromVueInstance.ts
+++ components/_util/getElementFromVueInstance.ts
@@ -2,12 +2,12 @@ import { isNil } from 'lodash-es';
 import type { ComponentOptions } from 'vue';
 
 /**
- * 如果传入的值是 Text 对象或 HTMLElement 对象，则直接返回；
+ * 如果传入的值是 Text 对象或 HTMLElement 对象或 SVG 对象，则直接返回；
  * 如果是 Vue 组件选项对象，则返回它的 DOM 元素；
  * 如果传入 null 参数，则返回 null。
  */
 export default function getElementFromVueInstance(
-    val?: HTMLElement | ComponentOptions | Text,
+    val?: HTMLElement | ComponentOptions | Text | SVGElement,
 ) {
     if (!val) {
         return null;
@@ -18,10 +18,13 @@ export default function getElementFromVueInstance(
     if (val instanceof HTMLElement) {
         return val;
     }
+    if (val instanceof SVGElement) {
+        return val;
+    }
     if (typeof val === 'object' && !isNil(val.$el)) {
         return val.$el;
     }
     throw new Error(
-        'getElementFromVueInstance 传入的值不是 Text、HTMLElement 或 Vue 组件选项对象',
+        'getElementFromVueInstance 传入的值不是 Text、HTMLElement、SVG 或 Vue 组件选项对象',
     );
 }
