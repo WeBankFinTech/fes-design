702a22e20e760e46803137579d3808b3e6ee7282
ocean_gao <gaopandyx@163.com>
2024-07-31 19:03:31 +0800

    docs(Popper): 解决控制台告警问题 (#871)

--- docs/.vitepress/components/popper/common.vue
+++ docs/.vitepress/components/popper/common.vue
@@ -1,34 +1,24 @@
 <template>
-    <FPopper placement="bottom" trigger="hover" :arrow="true" @trigger="handleTriggerChange">
+    <FPopper placement="bottom" trigger="hover" :arrow="true">
         <template #trigger>
             <FButton>Hover to activate</FButton>
         </template>
-        <div style="padding: 30px">
-            this is content, this is content, this is content
-        </div>
+        <div style="padding: 30px">this is content, this is content, this is content</div>
     </FPopper>
-    <FPopper placement="top" trigger="click" :arrow="true" @trigger="handleTriggerChange">
+    <FPopper placement="top" trigger="click" :arrow="true">
         <template #trigger>
             <FButton style="margin-left: 10px">Click to activate</FButton>
         </template>
-        <div style="padding: 30px">
-            this is content, this is content, this is content
-        </div>
+        <div style="padding: 30px">this is content, this is content, this is content</div>
     </FPopper>
     <FPopper placement="right" trigger="focus" :arrow="true">
         <template #trigger>
             <FButton style="margin-left: 10px">Focus to activate</FButton>
         </template>
-        <div style="padding: 30px">
-            this is content, this is content, this is content
-        </div>
+        <div style="padding: 30px">this is content, this is content, this is content</div>
     </FPopper>
 </template>
 
-<script setup lang="ts">
-const handleTriggerChange = (state: string) => {
-    console.log('[popper.common] handleTriggerChange, state:', state);
-};
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
