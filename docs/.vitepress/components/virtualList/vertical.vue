c7534832e88854530a1099764ac1db1e33d92e3a
harrywan <harrywan@webank.com>
2024-10-25 12:46:00 +0800

    fix: undo commit

--- docs/.vitepress/components/virtualList/vertical.vue
+++ docs/.vitepress/components/virtualList/vertical.vue
@@ -2,8 +2,8 @@
     <FVirtualList
         ref="virtualList"
         class="virtual-scroll-list-vertical"
-        :dataKey="(data) => data.id"
-        :dataSources="items"
+        dataKey="id"
+        :dataSources="dataItems"
         :estimateSize="80"
         :height="height"
     >
@@ -17,60 +17,67 @@
             </div>
         </template>
     </FVirtualList>
+
+    <FButton @click="addMessage">添加消息{{ dataItems.length }}</FButton>
 </template>
 
 <script>
 import { ref } from 'vue';
 
-function useDataItems() {
-    // The Climb (From Miley Cyrus)
-    const sentence3 = [
-        'BFC(Block formatting context)直译为"块级格式化上下文"。它是一个独立的渲染区域，只有Block-level box参与， 它规定了内部的Block-level Box如何布局，并且与这个区域外部毫不相干。',
-        'IFC(Inline Formatting Contexts)直译为”内联格式化上下文”，IFC的line box（线框）高度由其包含行内元素中最高的实际高度计算而来（不受到竖直方向的padding/margin影响)',
-        'margin 重合，margin 塌陷',
-        'css3',
-        'html5IFC(Inline Formatting Contexts)直译为”内联格式化上下文”，IFC的line box（线框）高度由其包含行内元素中最高的实际高度计算而来（不受到竖直方向的padding/margin影响)IFC(Inline Formatting Contexts)直译为”内联格式化上下文”，IFC的line box（线框）高度由其包含行内元素中最高的实际高度计算而来（不受到竖直方向的padding/margin影响)IFC(Inline Formatting Contexts)直译为”内联格式化上下文”，IFC的line box（线框）高度由其包含行内元素中最高的实际高度计算而来（不受到竖直方向的padding/margin影响)IFC(Inline Formatting Contexts)直译为”内联格式化上下文”，IFC的line box（线框）高度由其包含行内元素中最高的实际高度计算而来（不受到竖直方向的padding/margin影响)IFC(Inline Formatting Contexts)直译为”内联格式化上下文”，IFC的line box（线框）高度由其包含行内元素中最高的实际高度计算而来（不受到竖直方向的padding/margin影响)IFC(Inline Formatting Contexts)直译为”内联格式化上下文”，IFC的line box（线框）高度由其包含行内元素中最高的实际高度计算而来（不受到竖直方向的padding/margin影响)IFC(Inline Formatting Contexts)直译为”内联格式化上下文”，IFC的line box（线框）高度由其包含行内元素中最高的实际高度计算而来（不受到竖直方向的padding/margin影响)IFC(Inline Formatting Contexts)直译为”内联格式化上下文”，IFC的line box（线框）高度由其包含行内元素中最高的实际高度计算而来（不受到竖直方向的padding/margin影响)IFC(Inline Formatting Contexts)直译为”内联格式化上下文”，IFC的line box（线框）高度由其包含行内元素中最高的实际高度计算而来（不受到竖直方向的padding/margin影响)IFC(Inline Formatting Contexts)直译为”内联格式化上下文”，IFC的line box（线框）高度由其包含行内元素中最高的实际高度计算而来（不受到竖直方向的padding/margin影响)IFC(Inline Formatting Contexts)直译为”内联格式化上下文”，IFC的line box（线框）高度由其包含行内元素中最高的实际高度计算而来（不受到竖直方向的padding/margin影响)',
-        'es6',
-    ];
-
-    const TOTAL_COUNT = 1000;
-
-    const genUniqueId = (prefix) => {
-        return `${prefix}$${Math.random().toString(16).substr(9)}`;
-    };
-
-    const getSentences = () => {
-        const index = Math.floor(Math.random() * (sentence3.length - 1));
-        return sentence3[index];
-    };
-
-    const dataItems = ref([]);
-    let count = TOTAL_COUNT;
-    while (count--) {
-        const index = TOTAL_COUNT - count;
-        dataItems.value.push({
-            index,
-            name: `${Math.random()}`,
-            id: genUniqueId(index),
-            desc: getSentences(),
-        });
-    }
-
-    return dataItems;
-}
-
 export default {
     name: 'Vertical',
     setup() {
         const virtualList = ref(null);
-        const height = ref(200);
+        const height = ref(400);
 
-        const dataItems = useDataItems();
+        const sentence3 = [
+            'BFC(Block formatting context)直译为"块级格式化上下文"。它是一个独立的渲染区域，只有Block-level box参与， 它规定了内部的Block-level Box如何布局，并且与这个区域外部毫不相干。',
+            'IFC(Inline Formatting Contexts)直译为”内联格式化上下文”，IFC的line box（线框）高度由其包含行内元素中最高的实际高度计算而来（不受到竖直方向的padding/margin影响)',
+            'margin 重合，margin 塌陷',
+            'css3',
+            'html5IFC(Inline Formatting Contexts)直译为”内联格式化上下文”，IFC的line box（线框）高度由其包含行内元素中最高的实际高度计算而来（不受到竖直方向的padding/margin影响)IFC(Inline Formatting Contexts)直译为”内联格式化上下文”，IFC的line box（线框）高度由其包含行内元素中最高的实际高度计算而来（不受到竖直方向的padding/margin影响)IFC(Inline Formatting Contexts)直译为”内联格式化上下文”，IFC的line box（线框）高度由其包含行内元素中最高的实际高度计算而来（不受到竖直方向的padding/margin影响)IFC(Inline Formatting Contexts)直译为”内联格式化上下文”，IFC的line box（线框）高度由其包含行内元素中最高的实际高度计算而来（不受到竖直方向的padding/margin影响)IFC(Inline Formatting Contexts)直译为”内联格式化上下文”，IFC的line box（线框）高度由其包含行内元素中最高的实际高度计算而来（不受到竖直方向的padding/margin影响)IFC(Inline Formatting Contexts)直译为”内联格式化上下文”，IFC的line box（线框）高度由其包含行内元素中最高的实际高度计算而来（不受到竖直方向的padding/margin影响)IFC(Inline Formatting Contexts)直译为”内联格式化上下文”，IFC的line box（线框）高度由其包含行内元素中最高的实际高度计算而来（不受到竖直方向的padding/margin影响)IFC(Inline Formatting Contexts)直译为”内联格式化上下文”，IFC的line box（线框）高度由其包含行内元素中最高的实际高度计算而来（不受到竖直方向的padding/margin影响)IFC(Inline Formatting Contexts)直译为”内联格式化上下文”，IFC的line box（线框）高度由其包含行内元素中最高的实际高度计算而来（不受到竖直方向的padding/margin影响)IFC(Inline Formatting Contexts)直译为”内联格式化上下文”，IFC的line box（线框）高度由其包含行内元素中最高的实际高度计算而来（不受到竖直方向的padding/margin影响)IFC(Inline Formatting Contexts)直译为”内联格式化上下文”，IFC的line box（线框）高度由其包含行内元素中最高的实际高度计算而来（不受到竖直方向的padding/margin影响)',
+            'es6',
+        ];
+
+        const genUniqueId = (prefix) => {
+            return `${prefix}$${Math.random().toString(16).substr(9)}`;
+        };
+
+        const getSentences = () => {
+            const index = Math.floor(Math.random() * (sentence3.length - 1));
+            return sentence3[index];
+        };
+
+        const TOTAL_COUNT = 1000;
+
+        const dataItems = ref([]);
+        let count = TOTAL_COUNT;
+        while (count--) {
+            const index = TOTAL_COUNT - count;
+            dataItems.value.push({
+                index,
+                name: `${Math.random()}`,
+                id: genUniqueId(index),
+                desc: getSentences(),
+            });
+        }
+
+        const addMessage = () => {
+            const index = dataItems.value.length + 1;
+            dataItems.value = [...dataItems.value, {
+                index,
+                name: `${Math.random()}`,
+                id: genUniqueId(index),
+                desc: getSentences(),
+            }];
+            virtualList.value.scrollToBottom();
+        };
 
         return {
             virtualList,
-            items: dataItems,
+            dataItems,
             height,
+            addMessage,
         };
     },
 };
