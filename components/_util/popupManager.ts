c034308e3e0983e4db4ed2396b194fd63fc30396
ocean-gao <gaopandyx@163.com>
2024-11-01 10:00:59 +0800

    refactor: popupManager 代码规范

--- components/_util/popupManager.ts
+++ components/_util/popupManager.ts
@@ -1,19 +1,10 @@
 import { useSessionStorage } from '@vueuse/core';
 import { getPrefixStorage } from '../_util/storage';
 
-const INIT_Z_INDEX = 2000;
-
-const zIndex = useSessionStorage<number>(getPrefixStorage('z-index'), INIT_Z_INDEX);
-if (zIndex.value < INIT_Z_INDEX) {
-    zIndex.value = INIT_Z_INDEX;
-}
-
 const PopupManager = {
-    zIndex: zIndex.value,
+    zIndex: useSessionStorage<number>(getPrefixStorage('zIndex'), 2000),
     nextZIndex() {
-        ++zIndex.value;
-        PopupManager.zIndex = zIndex.value;
-        return PopupManager.zIndex;
+        return ++PopupManager.zIndex.value;
     },
 };
 
