0bdfadc3f24ad107c0059c141a2fd682db829457
ocean-gao <gaopandyx@163.com>
2024-11-01 09:46:41 +0800

    feat: zIndex 全局管理，由内存改为 sessionStorage

--- components/_util/popupManager.ts
+++ components/_util/popupManager.ts
@@ -1,7 +1,19 @@
+import { useSessionStorage } from '@vueuse/core';
+import { getPrefixStorage } from '../_util/storage';
+
+const INIT_Z_INDEX = 2000;
+
+const zIndex = useSessionStorage<number>(getPrefixStorage('z-index'), INIT_Z_INDEX);
+if (zIndex.value < INIT_Z_INDEX) {
+    zIndex.value = INIT_Z_INDEX;
+}
+
 const PopupManager = {
-    zIndex: 2000,
+    zIndex: zIndex.value,
     nextZIndex() {
-        return ++PopupManager.zIndex;
+        ++zIndex.value;
+        PopupManager.zIndex = zIndex.value;
+        return PopupManager.zIndex;
     },
 };
 
