36daca531d2a3fa328c38b46d28006bf9b8e1357
ocean-gao <gaopandyx@163.com>
2024-11-04 11:28:13 +0800

    refactor: 解决执行 build:type 提示类型警告的问题

--- components/_util/popupManager.ts
+++ components/_util/popupManager.ts
@@ -1,7 +1,11 @@
 import { useSessionStorage } from '@vueuse/core';
+import type { Ref } from 'vue';
 import { getPrefixStorage } from '../_util/storage';
 
-const PopupManager = {
+const PopupManager: {
+    zIndex: Ref<number>;
+    nextZIndex: () => number;
+} = {
     zIndex: useSessionStorage<number>(getPrefixStorage('zIndex'), 2000),
     nextZIndex() {
         return ++PopupManager.zIndex.value;
