370aff3bd22fc7d0c46b9b2192cefb4ad82e8430
harrywan <harrywan@webank.com>
2026-06-01 23:54:10 +0800

    test(layout): 为 #954 添加 vitest 单测、playwright e2e 和 CHANGELOG

---
+++ docs/.vitepress/components/layout/disabled.vue
@@ -0,0 +1,16 @@
+<template>
+    <f-layout :disabled="true">
+        <div class="login-page">Login Page Content</div>
+    </f-layout>
+</template>
+
+<style scoped>
+.login-page {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+    background: #f5f5f5;
+    font-size: 16px;
+}
+</style>
